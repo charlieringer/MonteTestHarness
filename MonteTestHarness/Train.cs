93944f085c6e304c28edcddbd17d88adede4b8d0
Charlie Ringer <cring002@gold.ac.uk>
2017-04-04 10:08:27 +0100

    Updating the test harness for QoL improvments (two differnt configs for Test/Train etc.)

---
+++ MonteTestHarness/Train.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Monte;
+
+public class Train
+{
+    public static void TrainMain()
+    {
+        Model model = new Model();
+        //model.train(100, 1000, () => new TTTAIState());
+        model.train(100, 100, () => new OCAIState());
+        Console.Write("Done");
+    }
+
+}
